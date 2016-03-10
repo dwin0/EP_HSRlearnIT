351e6ccc92503ebc5264698b90d228482eac69b9
dwin94 <dwindler@hsr.ch>
2016-03-10 11:21:13 +0100

    Prototype Classes
    - FileSaver.cs
    - AesGcmCryptoLibrary.cs

---
+++ EP_HSRlearnIT/AesGcmCryptoLibrary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EP_HSRlearnIT
+{
+    internal class AesGcmCryptoLibrary
+    {
+
+        internal String Encrypt(int key, string plaintext)
+        {
+            return "encrypted/ciphertext";
+        }
+
+        internal String Decrypt(int key, string ciphertext)
+        {
+            return "decrypted/plaintext";
+        }
+
+    }
+}
---
+++ EP_HSRlearnIT/FileSaver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EP_HSRlearnIT
+{
+    internal class FileSaver
+    {
+        internal bool SaveFile(String text)
+        {
+            return false;
+        }
+    }
+}
