2beab2660c81afd908563d0452d1adcd59be6aaf
Andi Eder <aeder@hsr.ch>
2016-04-01 15:52:04 +0200

    add Test for FileSaver

--- EP_HSRlearnIT/FileSaver.cs
+++ EP_HSRlearnIT/FileSaver.cs
@@ -10,12 +10,10 @@ namespace EP_HSRlearnIT
 {
     public class FileSaver
     {
-        private Boolean existFile = false;
-        private String topLevelFolder = @"c:\temp\HSRlearnIT";
-        private String fileName;
+        private String topLevelFolder = @"C:\temp\HSRlearnIT";
+        private String folderName = "Test";
         private String pathString;
-        //private int status = 0;
-        //private String currentContent;
+        private String fileName;
         #region Public Methods
 
         internal bool SaveFile(String text)
@@ -24,47 +22,47 @@ namespace EP_HSRlearnIT
         }
         #endregion
 
-        public FileSaver()
-        {
-        }
 
-        public void CreateFile(String name, int status, String currentContent)
+        public void CreateFile(String fileName, int status, String currentContent)
         {
-            if(existFile == false)
+            this.fileName = fileName;
+            pathString = Path.Combine(topLevelFolder, folderName, fileName);
+            if(!File.Exists(pathString))
             {
-                pathString = System.IO.Path.Combine(topLevelFolder, name);
-                System.IO.Directory.CreateDirectory(pathString);
-                fileName = System.IO.Path.GetRandomFileName();
-                pathString = System.IO.Path.Combine(pathString, fileName);
+
+                pathString = Path.Combine(topLevelFolder, folderName);
+                Directory.CreateDirectory(pathString);
+                pathString = Path.Combine(pathString, fileName);
+
                 Console.WriteLine("Filepath: {0}\n", pathString);
-                System.IO.FileStream file = System.IO.File.Create(pathString);
+                FileStream file = File.Create(pathString);
                 file.Close();
-                existFile = true;
-                WriteFile(status, currentContent);
-                ReadFile();
             }
+            WriteFile(status, currentContent);
+            String output = ReadFile();
+            //MessageBox.Show("The Content of the file is " + output);
         }
 
         public void WriteFile(int status, String currentContent)
         {
-            System.IO.StreamWriter file = new System.IO.StreamWriter(pathString, true);
+            StreamWriter file = new StreamWriter(pathString, true);
             file.Write(status);
             file.Write(currentContent);
             file.Close();
         }
 
-        public void ReadFile()
+        public String ReadFile()
         {
             FileInfo info = new FileInfo(pathString);
             StreamReader sr = new StreamReader(pathString);
             String output = sr.ReadToEnd();
-            MessageBox.Show("The Content of the file " + info.Name + " is " + output);
             sr.Close();
+            return output;
         }
 
         public void RemoveSaveFiles()
         {
-            System.IO.Directory.Delete(topLevelFolder);
+            Directory.Delete(topLevelFolder, true);
         }
     }
 }
---
+++ EP_HSRlearnITTests/FileSaverTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EP_HSRlearnIT;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EP_HSRlearnIT.Tests
+{
+    [TestClass()]
+    public class FileSaverTests
+    {
+        FileSaver newFile = new FileSaver();
+
+        [TestMethod()]
+        public void CreateFileTest()
+        {        
+            newFile.CreateFile("AES-GCM.txt", 20, " You are starting with the learntool! ");
+            Assert.IsTrue(System.IO.File.Exists(@"c:\temp\HSRlearnIT\Test\AES-GCM.txt"));
+        }
+
+        [TestMethod()]
+        public void ReadWriteFileTest()
+        {
+            newFile.CreateFile("RWTest.txt",20 , "");
+            newFile.WriteFile(30, "Hello Member!");
+            Assert.AreEqual("2030Hello Member!", newFile.ReadFile());
+        }
+
+        [TestMethod()]
+        public void RemoveSaveFilesTest()
+        {
+            newFile.CreateFile("RemoveTest.txt", 20, " You are starting with the learntool! ");
+            newFile.RemoveSaveFiles();
+            Assert.IsFalse(File.Exists(@"c:\temp\HSRlearnIT\Test\RemoveTest.txt"));
+        }
+    }
+}
