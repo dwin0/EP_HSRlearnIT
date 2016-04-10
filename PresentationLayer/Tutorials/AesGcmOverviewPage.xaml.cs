ee463ae2dac1fedf5710b8660be5c8b7fede4371
aha93 <a1hauser@hsr.ch>
2016-04-10 15:21:39 +0200

    AesGcmOverview MouseOver with ToolTip implemented

--- PresentationLayer/Tutorials/AesGcmOverviewPage.xaml.cs
+++ PresentationLayer/Tutorials/AesGcmOverviewPage.xaml.cs
@@ -14,5 +14,69 @@ namespace EP_HSRlearnIT.PresentationLayer.Tutorials
             InitializeComponent();
         }
         #endregion
+
+        #region Public Methods
+        private void Vorbereitung_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            ToolTip = "Erklärung zu Vorbereitung";
+        }
+
+        private void Vorbereitung_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            
+        }
+
+
+        private void Verschlüsselung_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            ToolTip = "Erklärung zu Verschlüsselung";
+        }
+
+        private void Verschlüsselung_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+
+        }
+
+        private void AdditionalAuthenticatedData_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            ToolTip = "Erklärung zu Additional Authenticated Data";
+        }
+
+        private void AdditionalAuthenticatedData_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+
+        }
+
+        private void CiphertextAuthentifizierung_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            ToolTip = "Erklärung zu Ciphertext Authentifizierung";
+        }
+
+        private void CiphertextAuthentifizierung_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+
+        }
+
+        private void Wiederholung_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            ToolTip = "Erklärung zu Wiederholung";
+        }
+
+        private void Wiederholung_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+
+        }
+
+        private void AbschlussUndRückgabe_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            ToolTip = "Erklärung zu Abschluss und Rückgabe";
+        }
+
+        private void AbschlussUndRückgabe_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+
+        }
+
+        #endregion Public Methods
     }
 }
