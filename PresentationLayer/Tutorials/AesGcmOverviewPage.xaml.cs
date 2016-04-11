653e0bb4c4e6b870d6efce3b85a3d90a4ec82114
aha93 <a1hauser@hsr.ch>
2016-04-11 20:35:04 +0200

    ToolTip changes position with mouse movement

--- PresentationLayer/Tutorials/AesGcmOverviewPage.xaml.cs
+++ PresentationLayer/Tutorials/AesGcmOverviewPage.xaml.cs
@@ -5,7 +5,7 @@ namespace EP_HSRlearnIT.PresentationLayer.Tutorials
     /// <summary>
     /// Interaction logic for AesGcmOverviewPage.xaml
     /// </summary>
-    public partial class AesGcmOverviewPage : Page
+    public partial class AesGcmOverviewPage
     {
         #region Constructors
 
@@ -15,68 +15,84 @@ namespace EP_HSRlearnIT.PresentationLayer.Tutorials
         }
         #endregion
 
-        #region Public Methods
+        #region Private Members
+        private readonly ToolTip _toolTip = new ToolTip();
+        #endregion Private Members
+
+        #region Private Methods
         private void Vorbereitung_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            ToolTip = "Erklärung zu Vorbereitung";
+            _toolTip.Content = "Erklärung zu Vorbereitung";
+            _toolTip.IsOpen = true;
+            _toolTip.StaysOpen = true;
         }
 
         private void Vorbereitung_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            
+            _toolTip.IsOpen = false;
         }
 
 
         private void Verschlüsselung_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            ToolTip = "Erklärung zu Verschlüsselung";
+            _toolTip.Content = "Erklärung zu Verschlüsselung";
+            _toolTip.IsOpen = true;
+            _toolTip.StaysOpen = true;
         }
 
         private void Verschlüsselung_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
         {
-
+            _toolTip.IsOpen = false;
         }
 
         private void AdditionalAuthenticatedData_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            ToolTip = "Erklärung zu Additional Authenticated Data";
+            _toolTip.Content = "Erklärung zu Additional Authenticated Data";
+            _toolTip.IsOpen = true;
+            _toolTip.StaysOpen = true;
         }
 
         private void AdditionalAuthenticatedData_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
         {
-
+            _toolTip.IsOpen = false;
         }
 
         private void CiphertextAuthentifizierung_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            ToolTip = "Erklärung zu Ciphertext Authentifizierung";
+            _toolTip.Content = "Erklärung zu Ciphertext Authentifizierung";
+            _toolTip.IsOpen = true;
+            _toolTip.StaysOpen = true;
         }
 
         private void CiphertextAuthentifizierung_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
         {
-
+            _toolTip.IsOpen = false;
         }
 
         private void Wiederholung_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            ToolTip = "Erklärung zu Wiederholung";
+            _toolTip.Content = "Erklärung zu Wiederholung";
+            _toolTip.IsOpen = true;
+            _toolTip.StaysOpen = true;
         }
 
         private void Wiederholung_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
         {
-
+            _toolTip.IsOpen = false;
         }
 
         private void AbschlussUndRückgabe_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            ToolTip = "Erklärung zu Abschluss und Rückgabe";
+            _toolTip.Content = "Erklärung zu Abschluss und Rückgabe";
+            _toolTip.IsOpen = true;
+            _toolTip.StaysOpen = true;
         }
 
         private void AbschlussUndRückgabe_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
         {
-
+            _toolTip.IsOpen = false;
         }
 
-        #endregion Public Methods
+        #endregion Private Methods
     }
 }
