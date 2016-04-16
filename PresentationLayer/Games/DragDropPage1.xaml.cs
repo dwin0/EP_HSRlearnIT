30daace3f442f5f64d84e2d1b9b98682ac427919
dwin94 <dwindler@hsr.ch>
2016-04-16 17:11:11 +0200

    Code Review DragDropPage1

--- PresentationLayer/Games/DragDropPage1.xaml.cs
+++ PresentationLayer/Games/DragDropPage1.xaml.cs
@@ -1,34 +1,25 @@
 ﻿using EP_HSRlearnIT.BusinessLayer.UniversalTools;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using EP_HSRlearnIT.BusinessLayer.UniversalTools;
 
 
 namespace EP_HSRlearnIT.PresentationLayer.Games
 {
-    public partial class DragDropPage1 : Page
+    public partial class DragDropPage1
     {
         #region Private Members
         private int _step = 1;
       
         #endregion
-
         public DragDropPage1()
         {
             InitializeComponent();
-            OrderImages();
+            SetImages(GetImages());
+            //SortImages();
 
             var progress = Progress.GetProgress("CurrentStep");
             if (progress != null)
@@ -38,10 +29,50 @@ namespace EP_HSRlearnIT.PresentationLayer.Games
 
           //  ReplaceText(_step);
         }
-        private void OrderImages()
+
+        private BitmapImage[] GetImages()
+        {
+            BitmapImage[] images = new BitmapImage[9];
+
+            for (int i = 1; i < 10; i++)
+            {
+                BitmapImage image = new BitmapImage(new Uri(@"/Images/dragdrop" + i + ".png", UriKind.RelativeOrAbsolute));
+                images[i-1] = image;
+            }
+
+            return images;
+        }
+
+        private void SetImages(BitmapImage[] images)
         {
             int i = 0;
-            Image[] images = { hashsubkey, iv, counter, multH, auth_data, auth_tag, ciphertext, plaintext, len };
+            foreach (var img in images)
+            {
+                Image image = new Image
+                {
+                    Source = img,
+                    Height = 35,
+                    Width = 80,
+                    AllowDrop = true,
+                    Name = "dragdrop" + i
+            };
+
+                image.Margin = new Thickness(600, -450 + i*100, image.Width, image.Height);
+                image.PreviewMouseLeftButtonDown += image_PreviewMouseLeftButtonDown;
+                image.MouseLeftButtonUp += image_PreviewMouseLeftButtonUp;
+                image.MouseMove += image_MouseMove;
+                Panel.SetZIndex(image, 2);
+
+                ControlGrid.Children.Add(image);
+                i++;
+            }
+        }
+
+        /*
+        private void SortImages()
+        {
+            int i = 0;
+            Image[] images = { Hashsubkey, Iv, Counter, MultH, AuthData, AuthTag, Ciphertext, Plaintext, Len };
             foreach (var img in images)
             {
                 img.Width = 80;
@@ -50,16 +81,18 @@ namespace EP_HSRlearnIT.PresentationLayer.Games
                 i++;
             }
         }
-        List<UIElement> AddedImages = new List<UIElement>();
+        */
 
-        bool _isMoving = false;
-        Image _imgMoved = null;
-        Thickness _marginStart;
+        private readonly List<UIElement> _addedImages = new List<UIElement>();
+
+        private bool _isMoving = false;
+        private Image _imgMoved;
+        //private Thickness _marginStart; //is never used
 
         // Position used for calculating mouse move
-        Point _previousMousePosition = new Point();
+        private Point _previousMousePosition;
 
-        Rect[] dropLocations =
+        private readonly Rect[] _dropLocations =
         {
             new Rect(-734, -166, 0, 0),
             new Rect(-537, -346, 0, 0),
@@ -87,28 +120,33 @@ namespace EP_HSRlearnIT.PresentationLayer.Games
             try
             {
                 //First it is checked which image is moved, then a new image is created  and set with height, width and margin, so it appears on the same position as the _imgMoved.
-                _imgMoved = (Image)sender;
-                Image finalImage = new Image();
-                finalImage.Width = 80;
-                finalImage.Height = 35;
-                finalImage.Margin = _imgMoved.Margin;
-                finalImage.Source = _imgMoved.Source;
-                finalImage.Name = "tmp" + _imgMoved.Name;
-                controlGrid.Children.Add(finalImage);
+                _imgMoved = sender as Image;
+                if (_imgMoved != null)
+                {
+                    Image finalImage = new Image
+                    {
+                        Width = 80,
+                        Height = 35,
+                        Margin = _imgMoved.Margin,
+                        Source = _imgMoved.Source,
+                        Name = "tmp" + _imgMoved.Name
+                    };
+                    ControlGrid.Children.Add(finalImage);
 
-                //Set Eventhandler for new image so new image behaves as original 
-                finalImage.PreviewMouseLeftButtonUp += image_PreviewMouseLeftButtonUp;
-                _imgMoved = finalImage;
+                    //Set Eventhandler for new image so new image behaves as original 
+                    finalImage.PreviewMouseLeftButtonUp += image_PreviewMouseLeftButtonUp;
+                    _imgMoved = finalImage;
+                }
 
                 // Remember the initial mouse position
                 _previousMousePosition = e.GetPosition(this);
-                _marginStart = _imgMoved.Margin;
+                //_marginStart = _imgMoved.Margin;
                 _isMoving = true;
             }
             catch (Exception ex)
             {
                 ExceptionLogger.WriteToLogfile(ex.Message, "image_PreviewMouseLeftButtonDown");
-                Console.WriteLine("Exception occured " + ex.Message);
+                //Console.WriteLine("Exception occured " + ex.Message); //wird dies benötigt?
             }
         }
 
@@ -121,27 +159,26 @@ namespace EP_HSRlearnIT.PresentationLayer.Games
                 //Check if the original image corresponds with one of the droplocations. If there is a intersection, then image can be dropped
                 Rect imageRect = new Rect(_imgMoved.Margin.Left, _imgMoved.Margin.Top, _imgMoved.Width, _imgMoved.Height);
                 bool bIntersection = false;
-                for (int i = 0; i < dropLocations.Length; i++)
+                foreach (Rect r in _dropLocations)
                 {
-                    Rect r = dropLocations[i];
                     if (imageRect.IntersectsWith(r))
                     {
                         bIntersection = true;
                         _imgMoved.Margin = new Thickness(r.Left - 80, r.Top - 35, 0, 0);
-                        AddedImages.Add(_imgMoved);
-                      //  Progress.SaveProgress( );
+                        _addedImages.Add(_imgMoved);
+                        //  Progress.SaveProgress( );
                         break;
                     }
                 }
                 if (!bIntersection)
                 {
-                    controlGrid.Children.Remove(_imgMoved);
+                    ControlGrid.Children.Remove(_imgMoved);
                 }
             }
             catch (Exception ex)
             {
                 ExceptionLogger.WriteToLogfile(ex.Message, "testMethod");
-                Console.WriteLine("Das Element konnte nicht korrekt zugeordnet werden. Fehlermeldung: " + ex.Message);
+                //Console.WriteLine("Das Element konnte nicht korrekt zugeordnet werden. Fehlermeldung: " + ex.Message); //wird dies benötigt?
             }
         }
 
@@ -160,17 +197,17 @@ namespace EP_HSRlearnIT.PresentationLayer.Games
                     oldMargin.Top += dragVertical * 2;
                     _imgMoved.Margin = oldMargin;
 
-                    //This part is only for debugging (so that the textbox gets showsn) and will be deleted for the final version
+                    //This part is only for debugging (so that the textbox gets shown) and will be deleted for the final version
                     Rect imageRect = new Rect(_imgMoved.Margin.Left, _imgMoved.Margin.Top, _imgMoved.Width, _imgMoved.Height);
                     bool bIntersection = false;
-                    for (int i = 0; i < dropLocations.Length; i++)
+                    foreach (var r in _dropLocations)
                     {
-                        Rect r = dropLocations[i];
                         if (imageRect.IntersectsWith(r))
 
                             bIntersection = true;
                     }
-                    textBox.Text = String.Format("X: {0}, Y:{1} INT: {2}", oldMargin.Left, oldMargin.Top, bIntersection ? "TRUE" : "FALSE");
+                    TextBox.Text = $"X: {oldMargin.Left}, Y:{oldMargin.Top} INT: {(bIntersection ? "TRUE" : "FALSE")}";
+                    
                 }
             }
             catch (Exception ex)
@@ -178,25 +215,33 @@ namespace EP_HSRlearnIT.PresentationLayer.Games
                 ExceptionLogger.WriteToLogfile(ex.Message, "testMethod");
             }
         }
-        private void reset_onclick(object sender, RoutedEventArgs e)
+        private void OnResetClick(object sender, RoutedEventArgs e)
         {
             try
             {
-                var images = controlGrid.Children.OfType<Image>().ToList();
+                foreach (var image in _addedImages)
+                {
+                    ControlGrid.Children.Remove(image);
+                }
+
+                /*
+                var images = ControlGrid.Children.OfType<Image>().ToList();
+                
                 foreach (var image in images)
                 {
                     if (image.Name.Contains("tmp"))
                     {
-                        controlGrid.Children.Remove(image);
+                        ControlGrid.Children.Remove(image);
                     }
                 }
-                AddedImages.Clear();
+                */
+                _addedImages.Clear();
+                
             }
             catch (Exception ex)
             {
                 ExceptionLogger.WriteToLogfile(ex.Message, "testMethod");
             }
-       
         }
     }
 }
