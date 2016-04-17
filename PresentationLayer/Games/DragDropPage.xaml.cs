be44d481762d5aad48f56e90b54c9daf8f867e73
Andi Eder <aeder@hsr.ch>
2016-04-17 18:58:14 +0200

    Revert "removed unused files"
    This reverts commit 3f2d25f709485d0a356beeff9810d1d5fca48dcd.

---
+++ PresentationLayer/Games/DragDropPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace EP_HSRlearnIT.PresentationLayer.Games
+{
+    /// <summary>
+    /// Interaction logic for DragDropPage.xaml
+    /// </summary>
+    public partial class DragDropPage : Page
+    {
+        #region Private Members
+
+        private ObservableCollection<AesGcmPart> _availableParts;
+        private ObservableCollection<AesGcmPart> _setParts;
+        private Point? _dragStartPosition = null;
+        #endregion
+
+
+        #region Constructors
+        public DragDropPage()
+        {
+            InitializeComponent();
+            InitParts();
+            DataContext = this;
+        }
+        #endregion
+
+        #region Private Methods
+
+        private void InitParts()
+        {
+            _availableParts = new ObservableCollection<AesGcmPart>();
+            _setParts = new ObservableCollection<AesGcmPart>();
+            new[] { "Part1", "Part2", "Part3" }
+                .ToList()
+                .ForEach(part => _availableParts.Add(new AesGcmPart() { Name = part, Image = $"/images/{part.ToLower()}.png" }));
+
+            AvailableListBox.ItemsSource = _availableParts;
+            SetListBox.ItemsSource = _setParts;
+        }
+
+        private void AvailableListBox_OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _dragStartPosition = e.GetPosition(this);
+        }
+
+        private void AvailableListBox_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _dragStartPosition = null;
+        }
+
+        private bool IsMovementFarEnough(Point origPos, Point curPos)
+        {
+            var minDistX = SystemParameters.MinimumHorizontalDragDistance;
+            var minDistY = SystemParameters.MinimumHorizontalDragDistance;
+
+            return (Math.Abs(curPos.X - origPos.X) >= minDistX
+                || Math.Abs(curPos.Y - origPos.Y) >= minDistY);
+        }
+
+        private void AvailableListBox_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            //Drag doesn't start if click wasn't withing available list
+            if(_dragStartPosition == null)
+            {
+                return;
+            }
+            //check if movement is far enough
+            var position = e.GetPosition(this);
+            if(!IsMovementFarEnough((Point)_dragStartPosition, position))
+            {
+                return;
+            }
+            //everything ok -> start Drag
+            _dragStartPosition = null;
+            StartDrag(AvailableListBox.SelectedItem as AesGcmPart);
+        }
+
+        private void StartDrag<T>(T obj)
+        {
+            var dragScope = Content as FrameworkElement;
+            var dragData = new DataObject(typeof(T), obj);
+            DragDrop.DoDragDrop(dragScope, dragData, DragDropEffects.Move);
+        }
+
+        private void SetListBox_OnDragOver(object sender, DragEventArgs e)
+        {
+            var data = e.Data.GetData(typeof(AesGcmPart)) as AesGcmPart;
+            //move object if available
+            if(data != null)
+            {
+                e.Effects = DragDropEffects.Copy;
+            } else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void SetListBox_OnDrop(object sender, DragEventArgs e)
+        {
+            var aesGcmPart = e.Data.GetData(typeof(AesGcmPart)) as AesGcmPart;
+            _setParts.Add(aesGcmPart);
+        }
+
+        private void OnResetButtonClick(object sender, RoutedEventArgs e)
+        {
+            _setParts.Clear();
+        }
+        #endregion
+    }
+}
---
+++ PresentationLayer/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using EP_HSRlearnIT.PresentationLayer.Tutorials;
+using EP_HSRlearnIT.PresentationLayer.Exercises;
+using EP_HSRlearnIT.PresentationLayer.Games;
+
+namespace EP_HSRlearnIT.PresentationLayer
+
+{
+
+    public partial class MainPage
+    {
+        #region Constructors
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+
+        #region Private Methods
+        private void OverviewScreen_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.Navigate(new AesGcmOverviewPage());
+        }
+
+        private void StepByStep_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.Navigate(new StepPage());
+        }
+
+        private void EncryptionDecryption_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.Navigate(new EncryptionDecyrptionPage());
+        }
+
+        private void DragAndDrop_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.Navigate(new DragDropPage1());
+        }
+        #endregion
+    }
+}
