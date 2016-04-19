5de923527c15e808f1f2f13f9826d841ed49415c
Curlie82 <sebnem.basar@gmail.com>
2016-04-19 10:23:25 +0200

    DragDrop Update
    Change of Mouse Cursor and Moving Elements

---
+++ PresentationLayer/Games/DragDropPageConstants.cs
@@ -0,0 +1,39 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Input;
+//using System.Windows.Media.Imaging;
+//using EP_HSRlearnIT.BusinessLayer.UniversalTools;
+
+
+//namespace EP_HSRlearnIT.PresentationLayer.Games
+//{
+//    public partial class DragDropPage
+//    {
+//        private readonly Rect[] _dropLocations ={
+//            new Rect(-728, -164, 0, 0),
+//            new Rect(-531, -348, 60, 35),
+//            new Rect(-531, -261,60, 35),
+//            new Rect(-732, -166, 60, 35),
+//            new Rect(-346, -95, 60, 35),
+//            new Rect(-379, 131, 60, 35),
+//            new Rect(-379, 220, 60, 35),
+//            new Rect(-142, -262,60, 35),
+//            new Rect(-142, -166, 60, 35),
+//            new Rect(-140, 129, 60, 35),
+//            new Rect(-137, -14,60, 35),
+//            new Rect(-150, 126, 60, 35),
+//            new Rect(-331, -98, 60, 35),
+//            new Rect(104, -106, 60, 35),
+//            new Rect(104, 212, 60, 35),
+//            new Rect(96, 383, 60, 35),
+//            new Rect(292, -261, 60, 35),
+//            new Rect(292, -166, 60, 35),
+//            new Rect(289, -29, 60, 35),
+//            new Rect(289, 107,60, 35),
+//            new Rect(289, 298, 60, 35),
+//            new Rect(289, 458, 60, 35),
+//        };
+//    }
+//}
