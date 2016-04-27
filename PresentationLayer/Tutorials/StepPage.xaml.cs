21f0d3ed9bb3eeb26345566e28f15c9e52cf8f0c
Andi Eder <aeder@hsr.ch>
2016-04-27 08:16:34 +0200

    * formated all text for description * add text for title * implement change title

--- PresentationLayer/Tutorials/StepPage.xaml.cs
+++ PresentationLayer/Tutorials/StepPage.xaml.cs
@@ -10,6 +10,7 @@ namespace EP_HSRlearnIT.PresentationLayer.Tutorials
     {
         #region Private Members
         private int _step;
+        private int _title;
         private const int StepMin = 0;
         private const int StepMax = 18;
 
@@ -20,10 +21,17 @@ namespace EP_HSRlearnIT.PresentationLayer.Tutorials
         public StepPage()
         {
             InitializeComponent();
+
             var progressCurrentStep = Progress.GetProgress("StepPage_CurrentStep");
             if(progressCurrentStep != null)
             {
                 _step = Convert.ToInt32(progressCurrentStep);      
+            }
+
+            var progressTitleStep = Progress.GetProgress("StepPage_Title");
+            if (progressTitleStep != null)
+            {
+                _title = Convert.ToInt32(progressTitleStep);
             }
 
             ReplaceContent(_step);
@@ -43,12 +51,14 @@ namespace EP_HSRlearnIT.PresentationLayer.Tutorials
         {
             ReplaceContent(--_step);
             Progress.SaveProgress("StepPage_CurrentStep", _step);
+            Progress.SaveProgress("StepPage_Title", _title);
         }
 
         private void OnNextStepButton_Click(object sender, RoutedEventArgs e)
         {
             ReplaceContent(++_step);
             Progress.SaveProgress("StepPage_CurrentStep", _step);
+            Progress.SaveProgress("StepPage_Title", _title);
         }
 
         private void OnStartDragDropButton_Click(object sender, RoutedEventArgs e)
@@ -83,7 +93,40 @@ namespace EP_HSRlearnIT.PresentationLayer.Tutorials
 
             StepDescriptionBox.Text = Application.Current.FindResource("Step" + stepNumber) as string;
             StepImage.Source = new BitmapImage(new Uri(@"/Images/Step" + stepNumber + ".png", UriKind.RelativeOrAbsolute));
-            StepTitle.Text = "Schritt " + stepNumber;
+            StepTitle.Text = WriteTitle(stepNumber);
+        }
+
+        private string WriteTitle(int stepNumber)
+        {
+            string title = GetTitle(stepNumber);
+            if (title == null)
+            {
+                string oldTitle = GetTitle(_title);
+                if (oldTitle == null || stepNumber < _title)
+                {
+                    for (int i = stepNumber; i >= StepMin; i--)
+                    {
+                        title = GetTitle(i);
+                        if (title != null)
+                        {
+                            return title;
+                        }
+                    }
+                }
+                return oldTitle;
+            }
+            _title = stepNumber;
+            return title;
+        }
+
+        private string GetTitle(int titleNumber)
+        {
+            string titleName = "Step" + titleNumber + "Title";
+            if(Application.Current.Resources.Contains(titleName))
+            {
+                return Application.Current.FindResource(titleName) as string;
+            }
+            return null;
         }
 
         #endregion
