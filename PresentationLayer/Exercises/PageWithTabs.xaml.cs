717771389ad0a62b1dd8f6d8c187cd81157a74c3
aha93 <a1hauser@hsr.ch>
2016-05-11 10:10:55 +0200

    Encryption and Decryption, the changes are now propagated between the tabs

--- PresentationLayer/Exercises/PageWithTabs.xaml.cs
+++ PresentationLayer/Exercises/PageWithTabs.xaml.cs
@@ -1,5 +1,5 @@
-﻿using System.Windows.Controls;
-using EP_HSRlearnIT.PresentationLayer.Exercises;
+﻿using System;
+using System.Windows.Controls;
 using EP_HSRlearnIT.BusinessLayer.UniversalTools;
 
 namespace EP_HSRlearnIT.PresentationLayer.Exercises
@@ -10,6 +10,25 @@ namespace EP_HSRlearnIT.PresentationLayer.Exercises
         public PageWithTabs()
         {
             InitializeComponent();
+            EncryptionFrame.Source = new Uri("EncryptionPage.xaml", UriKind.RelativeOrAbsolute);
+        }
+
+        public PageWithTabs(TabItem tabItem)
+        {
+            InitializeComponent();
+            
+            if (tabItem.Name == "EncryptionItem")
+            {
+                EncryptionItem.IsSelected = true;
+                DecryptionItem.IsSelected = false;
+                EncryptionFrame.Source = new Uri("EncryptionPage.xaml", UriKind.RelativeOrAbsolute);
+            }
+            else if (tabItem.Name == "DecryptionItem")
+            {
+                EncryptionItem.IsSelected = false;
+                DecryptionItem.IsSelected = true;
+                DecryptionFrame.Source = new Uri("DecryptionPage.xaml", UriKind.RelativeOrAbsolute);
+            }
         }
 
         #endregion
@@ -21,50 +40,62 @@ namespace EP_HSRlearnIT.PresentationLayer.Exercises
             {
                 if (sendingPage.Title == "EncryptionPage")
                 {
-                    foreach (var element in DependencyObjectExtension.GetAllChildren<TextBox>(this))
-                    {
-                        if (element.Name == "HexCiphertextBox")
-                        {
-                            string ciphertext = element.Text;
-                            Progress.SaveProgress("DecryptionPage_HexCiphertextBox", ciphertext);
-                        }
-                        else if (element.Name == "HexAadBox")
-                        {
-                            string aad = element.Text;
-                            Progress.SaveProgress("DecryptionPage_HexAadBox", aad);
-                        }
-                        else if (element.Name == "HexIvBox")
-                        {
-                            string iv = element.Text;
-                            Progress.SaveProgress("DecryptionPage_HexIvBox", iv);
-                        }
-                        else if (element.Name == "HexTagBox")
-                        {
-                            string tag = element.Text;
-                            Progress.SaveProgress("DecryptionPage_HexTagBox", tag);
-                        }
-                    }
+                    TakeValuesToDecryption();
+                    NavigationService?.Navigate(new PageWithTabs(DecryptionItem));
                 } else if (sendingPage.Title == "DecryptionPage")
                 {
-                    foreach (var element in DependencyObjectExtension.GetAllChildren<TextBox>(this))
-                    {
-                        if (element.Name == "HexPlaintextBox")
-                        {
-                            string plaintext = element.Text;
-                            Progress.SaveProgress("EncryptionPage_HexPlaintextBox", plaintext);
-                        }
-                        else if (element.Name == "HexIvBox")
-                        {
-                            string iv = element.Text;
-                            Progress.SaveProgress("EncryptionPage_HexIvBox", iv);
-                        }
-                        else if (element.Name == "HexAadBox")
-                        {
-                            string aad = element.Text;
-                            Progress.SaveProgress("EncryptionPage_HexAadBox", aad);
-                        }
-                    }
+                    TakeValuesToEncryption();
+                    NavigationService?.Navigate(new PageWithTabs(EncryptionItem));
                 } 
+            }
+        }
+
+        private void TakeValuesToDecryption()
+        {
+            foreach (var element in DependencyObjectExtension.GetAllChildren<TextBox>(this))
+            {
+                if (element.Name == "HexCiphertextBox")
+                {
+                    string ciphertext = element.Text;
+                    Progress.SaveProgress("DecryptionPage_HexCiphertextBox", ciphertext);
+                }
+                else if (element.Name == "HexAadBox")
+                {
+                    string aad = element.Text;
+                    Progress.SaveProgress("DecryptionPage_HexAadBox", aad);
+                }
+                else if (element.Name == "HexIvBox")
+                {
+                    string iv = element.Text;
+                    Progress.SaveProgress("DecryptionPage_HexIvBox", iv);
+                }
+                else if (element.Name == "HexTagBox")
+                {
+                    string tag = element.Text;
+                    Progress.SaveProgress("DecryptionPage_HexTagBox", tag);
+                }
+            }
+        }
+
+        private void TakeValuesToEncryption()
+        {
+            foreach (var element in DependencyObjectExtension.GetAllChildren<TextBox>(this))
+            {
+                if (element.Name == "HexPlaintextBox")
+                {
+                    string plaintext = element.Text;
+                    Progress.SaveProgress("EncryptionPage_HexPlaintextBox", plaintext);
+                }
+                else if (element.Name == "HexIvBox")
+                {
+                    string iv = element.Text;
+                    Progress.SaveProgress("EncryptionPage_HexIvBox", iv);
+                }
+                else if (element.Name == "HexAadBox")
+                {
+                    string aad = element.Text;
+                    Progress.SaveProgress("EncryptionPage_HexAadBox", aad);
+                }
             }
         }
 
