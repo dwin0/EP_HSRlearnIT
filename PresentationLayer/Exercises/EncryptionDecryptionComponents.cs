f6176e97b427c5777178e42be7a143e7695b523e
Andi Eder <aeder@hsr.ch>
2016-05-17 14:27:11 +0200

    catch abort button

--- PresentationLayer/Exercises/EncryptionDecryptionComponents.cs
+++ PresentationLayer/Exercises/EncryptionDecryptionComponents.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -206,7 +207,17 @@ namespace EP_HSRlearnIT.PresentationLayer.Exercises
                 Title = "Importieren der Entschlüsselungsparameter"
             };
 
-            if (openFileDialog.ShowDialog() == true)
+            openFileDialog.FileOk += ImportData;
+            openFileDialog.ShowDialog();
+
+        }
+
+        private void ImportData(object sender, EventArgs e)
+        {
+            var openFileDialog = sender as OpenFileDialog;
+//            if (openFileDialog.ShowDialog() == true)
+//            {
+            if (openFileDialog != null)
             {
                 string filePath = openFileDialog.FileName;
                 IEnumerable<string> allLines = FileManager.ReadAllLines(filePath);
@@ -218,6 +229,7 @@ namespace EP_HSRlearnIT.PresentationLayer.Exercises
                     string parameter = line.Substring(0, index - 1);
                     string value = line.Substring(index + 1);
                     FillingField(parameter, value);
+//                }
                 }
             }
             MessageBox.Show("Der Import wurde erfolgreich abgeschlossen.", "Import einer Parameterdatei", MessageBoxButton.OK, MessageBoxImage.Information);
