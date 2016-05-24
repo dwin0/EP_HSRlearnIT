fad2572671837e5a7320d243936aee5c47af2829
dwin94 <dwindler@hsr.ch>
2016-05-24 22:59:56 +0200

    Implemented Class for Common Tutorial Methods

---
+++ PresentationLayer/Tutorials/TutorialsComponents.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace EP_HSRlearnIT.PresentationLayer.Tutorials
+{
+    /// <summary>
+    /// Contains all common Methods of Tutorial-Pages
+    /// </summary>
+    public class TutorialsComponents : Page
+    {
+        //TODO: Put Methods here
+    }
+}
